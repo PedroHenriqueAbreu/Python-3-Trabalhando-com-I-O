87915a2752c50573d5d05c473c43ca25543c6b3b
Pedro Henrique <alohapedrohenrique@gmail.com>
2021-04-14 10:05:42 -0400

    Lendo o arquivo linha a linha

--- principal.py
+++ principal.py
@@ -1,7 +1,5 @@
 
 arquivo_contatos = open ('dados/contatos.csv', encoding = 'latin_1')
 
-conteudo = arquivo_contatos.readlines()
-
-for linha in conteudo:
+for linha in arquivo_contatos:
     print(linha, end='')
