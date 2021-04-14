35b16d66fb1b3df0ff704c771f9c1eeb8a802fee
Pedro Henrique <alohapedrohenrique@gmail.com>
2021-04-14 16:31:50 -0400

    except FileNotFoundError e except PermissionError

--- principal.py
+++ principal.py
@@ -1,7 +1,13 @@
 try:
-    arquivo_contatos = open ('dados/contatos.csv', encoding = 'latin_1')
+    arquivo_contatos = open ('dados/nao-existe.csv', encoding = 'latin_1', mode='w+')
 
     for linha in arquivo_contatos:
         print(linha, end='')
+
+except FileNotFoundError:
+    print('Arquivo não encontrado')
+except PermissionError:
+    print('Sem permissão de escrita')
+
 finally:
     arquivo_contatos.close()
