7cd67cfd8bfda171955e8668fbf1a6b1329fb4e2
Pedro Henrique <alohapedrohenrique@gmail.com>
2021-04-14 16:40:57 -0400

    No arquivo principal.py e testa_concorrencia, ao inves de abrir com o open(), abrimos com whit open

--- principal.py
+++ principal.py
@@ -1,13 +1,10 @@
 try:
-    arquivo_contatos = open ('dados/nao-existe.csv', encoding = 'latin_1', mode='w+')
-
-    for linha in arquivo_contatos:
-        print(linha, end='')
+    with open('dados/contatos.csv', encoding='latin_1') as arquivo_contatos:
+        for linha in arquivo_contatos:
+            print(linha, end='')
 
 except FileNotFoundError:
     print('Arquivo não encontrado')
+
 except PermissionError:
     print('Sem permissão de escrita')
-
-finally:
-    arquivo_contatos.close()
